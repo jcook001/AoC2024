2a28ff8848aa08ea7662ff4734ec024381235a94
Jamie Cook <cookj024@gmail.com>
2024-12-07 15:30:11 +0000

    Added Day07 files - not yet started

---
+++ Days/Day07.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AoC2024.Days
+{
+    [SupportedOSPlatform("windows7.0")] //Added to stop copy to clipboard function complaining
+    internal class Day07
+    {
+        [STAThread]
+        public static void Solve()
+        {
+            /////////////////////////////////////////////
+            /////////////////* Part 1 *//////////////////
+            /////////////////////////////////////////////
+
+            String[] input = Utils.ReadInput();
+            if (input == null)
+            {
+                Console.WriteLine("Press any key to close");
+                Console.ReadKey();
+                return;
+            }
+
+            foreach (string s in input)
+            {
+                String[] tempStringArray = s.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            
+
+
+
+            int answer = 0;
+            // Set the Foreground color to yellow - helpful for highlighting answer
+            Console.ForegroundColor
+                = ConsoleColor.Yellow;
+            Console.WriteLine("Part 1 answer is {0}", answer);
+            if (answer != 0)
+            {
+                Clipboard.SetText(answer.ToString());
+                Console.WriteLine("Answer copied to clipboard!");
+            }
+
+            // Set the Foreground color to white - set back to white for debug text
+            Console.ForegroundColor
+                = ConsoleColor.White;
+
+            /////////////////////////////////////////////
+            /////////////////* Part 2 *//////////////////
+            /////////////////////////////////////////////
+
+
+
+            int answer2 = 0;
+
+            // Set the Foreground color to yellow  - helpful for highlighting answer
+            Console.ForegroundColor
+                = ConsoleColor.Yellow;
+            if (answer2 != 0)
+            {
+                Console.WriteLine("Part2 answer is = " + answer2);
+                Clipboard.SetText(answer2.ToString());
+                Console.WriteLine("Answer copied to clipboard!");
+            }
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+
+    }
+}
